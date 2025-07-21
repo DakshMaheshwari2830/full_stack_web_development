e97f27fe678aabfdc5f6554a8bc5fb24f157894a
DakshMaheshwari2830 <dakshmaheshwari2807@gmail.com>
2025-07-21 17:29:33 +0530

    rename files

---
+++ JQuery/index.js
@@ -0,0 +1,35 @@
+// $("h1").addClass("big-title margin-50");
+
+// $("h1").text("Bye");
+// $("button").html("Don't click me");
+
+
+// $("button").click(function() {
+//     $("h1").css("color", "purple");
+// });
+
+// $("button").click(function() {
+//        $("h1").hide();
+// });
+
+// $("button").click(function() {
+//        $("h1").toggle();
+// });
+
+
+// $("button").click(function() {
+//        $("h1").fadeout/fadetoggle();
+// });
+
+//  $("button").click(function() {
+//     $("h1").slideup/slidetoggle();
+//  });    
+
+//  $("button").click(function() {
+//        $("h1").animate({capacity: 0.5}); //inside the animate it contains only numberic values
+// });
+
+$("button")click(function() {
+       $("h1").slideUp().slideDown().animate({opacity:0.5});
+});
+
